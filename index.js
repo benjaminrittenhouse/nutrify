9906373d9258e49ee7604dcb0b4f2b836ef2e0ea
benjaminrittenhouse <67079818+benjaminrittenhouse@users.noreply.github.com>
2022-03-31 00:51:46 -0400

    heroku port modification

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.use(express.static('public'))
               var arr = getMyData(data2);
               var artists = getArtists(data2);
               var times = getDuration(data2);
-              res.render("main", {layout : "index", trackone : arr[0], tracktwo : arr[1], trackthree : arr[2], artists1: artists[0], artists2: artists[1], artists3: artists[2], timeone: times[0], timetwo: times[1], timethree: times[2], timetotal: times[3], date: times[4]});
+              res.render("index", {layout : "index", trackone : arr[0], tracktwo : arr[1], trackthree : arr[2], artists1: artists[0], artists2: artists[1], artists3: artists[2], timeone: times[0], timetwo: times[1], timethree: times[2], timetotal: times[3], date: times[4]});
            }};
 
         xhr2.send();
